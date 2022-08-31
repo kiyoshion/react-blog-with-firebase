4e41e707332d79dc8efb160f8bd625308d8630b3
kiyoshion <kiyoshion@gmail.com>
2022-08-31 20:03:31 +0900

    Add auth check in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,18 @@ import React from 'react'
 import { Link } from "react-router-dom"
 import "./Navbar.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faFilePen, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faFilePen, faArrowAltCircleRight, faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons'
 
-const Navbar = () => {
+const Navbar = ({ isAuth }) => {
   return (
     <nav>
       <Link to="/"><FontAwesomeIcon icon={faHouse} />ホーム</Link>
       <Link to="/createpost"><FontAwesomeIcon icon={faFilePen} />記事投稿</Link>
-      <Link to="/login"><FontAwesomeIcon icon={faArrowAltCircleRight} />ログイン</Link>
+      {!isAuth ? (
+          <Link to="/login"><FontAwesomeIcon icon={faArrowAltCircleRight} />ログイン</Link>
+        ) : (
+          <Link to="/logout"><FontAwesomeIcon icon={faArrowAltCircleLeft} />ログアウト</Link>
+      )}
     </nav>
   )
 }
