8926ac892cca992bf277f2715e100e5a47788aa7
kiyoshion <kiyoshion@gmail.com>
2022-08-31 21:06:34 +0900

    Add getPosts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postBody">
+          hoge
+        </div>
+        <div className="postFooter">
+          <div className="postAuther">
+            fuga
+          </div>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
